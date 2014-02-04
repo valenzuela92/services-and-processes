08ba3ef0169de64c18858e143dc751e0436ddab2
estefania <estefania@estefania-VirtualBox.(none)>
2014-02-04 10:10:38 +0100

    second daemon

---
+++ solutions/ruby/daemon2.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/ruby -w
+# daemon_spawn.rb
+require 'tempfile'
+require ' 
+daemonize'
+include Daemonize
+puts "About to daemonize."
+fork do
+  daemonize
+  log = Tempfile.new('daemon.log')
+  loop do
+    log.puts "I'm a daemon, doin' daemon things."
+    log.flush
+    sleep 5
+  end
+end
+puts 'The subprocess has become a deamon.'
+puts "But I'm going to stick around for a while."
+sleep 10
+puts "Okay, now I'm done."
